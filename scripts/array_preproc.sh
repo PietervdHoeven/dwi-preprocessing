c3af011ccf55d984a292c104280b7ed47b9c8cfd
PietervdHoeven <pietervanderhoeven98@gmail.com>
2025-05-25 13:19:21 +0200

    gitignore logs/

--- scripts/array_preproc.sh
+++ scripts/array_preproc.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=18
 #SBATCH --mem=120G
-#SBATCH --time=00:02:30
+#SBATCH --time=01:00:00
 #SBATCH --output=logs/slurm_%A_%a.out
 #SBATCH --error=logs/slurm_%A_%a.err
 
@@ -14,15 +14,17 @@ set -euo pipefail
 BASE_DIR=${HOME}/dwi-preprocessing/data
 SCRIPTS_DIR=${HOME}/dwi-preprocessing/scripts
 ORIG_CONTAINER=${HOME}/dwi-preprocessing/containers/neurotools.img
-SES_LIST=${BASE_DIR}/ses_list.txt
+SES_LIST=${BASE_DIR}/sessions_a.txt
 
 # 2) FIGURE OUT WHICH SESSION TO RUN
 LINE=$( sed -n "${SLURM_ARRAY_TASK_ID}q;d" "$SES_LIST" )
 P_ID=${LINE%%/*}
 S_ID=${LINE##*/}
 
+echo $P_ID
+echo $S_ID
+
 # 3) STAGE INTO LOCAL SCRATCH
-# Use SLURM tmpdir if available, otherwise node-local scratch
 mkdir -p "${TMPDIR}/oasis-data/${P_ID}/${S_ID}"
 
 # copy only that subject/session data
@@ -47,4 +49,4 @@ cp -r "${TMPDIR}/preproc/${P_ID}/${S_ID}" \
       "${BASE_DIR}/preproc/${P_ID}/${S_ID}"
 
 # 6) CLEAN UP (optional)
-rm -rf "${TMPDIR}"
+# rm -rf "${TMPDIR}"
